5f8d134f032ab71ff92a9977e7ebebe3af2afc1a
naglicmoc <dvd.filmas@gmail.com>
2025-10-16 20:52:14 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+print("hi")
+print("sveiki studentai")
+print("uzsakymai", "agrokultura", 'peugeot 807')
+
+print("vienas " + " tekstas")
+
+vardas = 'Petras'
+
+print(vardas)
+
+amzius = 24
+
+is_man = True
+should_eat_after_lesson = False
+has_cat = False
+
+print(str(7 + 10) + "labas")
+print(7 * 'labas ;) ')
+
+
+
+
+
+
+
+
+
+
